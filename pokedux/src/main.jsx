13601136fa970a9cb03361870d39d9e8ad5f73d1
Alonso <alonsonh94@gmail.com>
2025-01-28 00:27:34 -0600

    Add redux dev tools

--- pokedux/src/main.jsx
+++ pokedux/src/main.jsx
@@ -6,7 +6,10 @@ import App from "./Pages/App";
 import { pokemonsReducer } from "./redux/reducers";
 import { Provider } from "react-redux";
 
-const store = createStore(pokemonsReducer);
+const store = createStore(
+  pokemonsReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
