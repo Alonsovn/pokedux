3554674f20d45d496f6c10b35f4e07a0f46e59fc
Alonso <alonsonh94@gmail.com>
2025-01-28 00:14:17 -0600

    Implement redux

--- pokedux/src/Pages/App/index.jsx
+++ pokedux/src/Pages/App/index.jsx
@@ -2,15 +2,37 @@ import { Col } from "antd";
 import { Searcher } from "../../Components/Searcher";
 import "./App.css";
 import { PokemonList } from "../../Components/PokemonList";
+import logo from "../../statics/logo.svg";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getPokemon } from "../../redux/api";
+import { setPokemons } from "../../redux/actions";
 
 function App() {
+  const pokemons = useSelector((state) => state.pokemons);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const fetchPokemons = async () => {
+      const pokemonsRes = await getPokemon();
+      dispatch(setPokemons(pokemonsRes));
+    };
+
+    fetchPokemons();
+  }, []);
+
   return (
     <>
       <div className="App">
+        <Col span={4} offset={10}>
+          <img src={logo} alt="Pokedux" />
+        </Col>
+
         <Col span={8} offset={8}>
           <Searcher />
         </Col>
-        <PokemonList />
+
+        <PokemonList pokemons={pokemons} />
       </div>
     </>
   );
--- pokedux/src/main.jsx
+++ pokedux/src/main.jsx
@@ -1,10 +1,17 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { legacy_createStore as createStore } from "redux";
 import "./index.css";
 import App from "./Pages/App";
+import { pokemonsReducer } from "./redux/reducers";
+import { Provider } from "react-redux";
+
+const store = createStore(pokemonsReducer);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </StrictMode>
 );
