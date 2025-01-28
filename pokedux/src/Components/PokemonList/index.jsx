f69083c71612f425e13fa3cceda4f5a04b3060d2
Alonso <alonsonh94@gmail.com>
2025-01-27 23:57:20 -0600

    Add PokemonList component

--- pokedux/src/Components/PokemonList/index.jsx
+++ pokedux/src/Components/PokemonList/index.jsx
@@ -1,17 +1,9 @@
 import { PokemonCard } from "../PokemonCard";
 import "./PokemonList.css";
 
-const PokemonList = () => {
-  //create a pokemon list with 10 elements
-  const pokemons = Array.from({ length: 10 }, (_, index) => ({
-    id: index,
-    name: `Pokemon ${index + 1}`,
-    image:
-      "https://allroundclub.com/blog/wp-content/uploads/2021/10/how-to-draw-pikachu.png",
-    description: "fire , magic",
-  }));
+const PokemonList = ({ pokemons }) => {
   return (
-    <div className="PokemomList">
+    <div className="PokemonList">
       {pokemons.map((pokemon) => (
         <PokemonCard key={pokemon.id} pokemon={pokemon} />
       ))}
