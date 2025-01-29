4cc79125d9e93c9e37d2b560f3e85cc9c18498fc
Alonso <alonsonh94@gmail.com>
2025-01-28 19:06:41 -0600

    Adding custom middleware

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,22 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { legacy_createStore as createStore } from "redux";
+import {
+  applyMiddleware,
+  compose,
+  legacy_createStore as createStore,
+} from "redux";
 import "./index.css";
 import App from "./Pages/App";
 import { pokemonsReducer } from "./redux/reducers";
 import { Provider } from "react-redux";
+import { featuring, logger } from "./middleware";
 
-const store = createStore(
-  pokemonsReducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+const composedEnhancer = compose(
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+  applyMiddleware(logger, featuring)
 );
+
+const store = createStore(pokemonsReducer, composedEnhancer);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
